25e2448cde517ae4c55b861f33816f3547c5ff13
Stella L Lage <stellallage@hotmail.com>
2024-02-21 21:37:55 -0300

    Store Item splitted from StoreSettings

---
+++ Assets/_Scripts/StoreItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class StoreItem
+{
+    [SerializeField] public string Name;
+    [SerializeField] public string Description;
+    [SerializeField] public Sprite Icon;
+    [SerializeField] public int PurchaseCost;
+    [SerializeField] public List<int> UpgradeCost;
+    [SerializeField] public float Multiplier;
+    [SerializeField] public int MaxLevel;
+
+    public int GetCurrentUpgradeCost(int currentLevel)
+    {
+        return currentLevel > UpgradeCost.Count ? UpgradeCost[^1] : UpgradeCost[currentLevel];
+    }
+}
--- Assets/_Scripts/StoreSettings.cs
+++ Assets/_Scripts/StoreSettings.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,21 +6,4 @@ using UnityEngine;
 public class StoreSettings : ScriptableObject
 {
     public List<StoreItem> StoreItems = new List<StoreItem>();
-}
-
-[Serializable]
-public class StoreItem
-{
-    [SerializeField] public string Name;
-    [SerializeField] public string Description;
-    [SerializeField] public Sprite Icon;
-    [SerializeField] public int PurchaseCost;
-    [SerializeField] public List<int> UpgradeCost;
-    [SerializeField] public float Multiplier;
-    [SerializeField] public int MaxLevel;
-
-    public int GetCurrentUpgradeCost(int currentLevel)
-    {
-        return currentLevel > UpgradeCost.Count ? UpgradeCost[^1] : UpgradeCost[currentLevel];
-    }
 }
