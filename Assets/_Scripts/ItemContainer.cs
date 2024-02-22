8e7a03392e11a4b590dc831d58b9495356dda7a7
Stella L Lage <stellallage@hotmail.com>
2024-02-22 14:22:35 -0300

    Created abstract classs ItemContainer

---
+++ Assets/_Scripts/ItemContainer.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class ItemContainer : MonoBehaviour
+{
+    protected int _currentLevel { get; private set; }
+    protected virtual void SetName(string itemName) { }
+    protected virtual void SetIcon(Sprite icon) {} 
+    protected void SetCurrentLevel(int currentLevel) => _currentLevel = currentLevel;
+    protected void AddLevel() => _currentLevel += 1;
+    
+    protected virtual void OnClicked() { }
+    
+    public virtual void Reset() => SetCurrentLevel(0);
+}
